b817e45bc12c3b4f34e076f40c04ed9adb860f3f
Ali Mohamed <alimohamed351@outlook.com>
2025-05-13 13:39:53 -0700

    finished challenge

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll('.exp');
-
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             const paragraph = this.closest('.tile').querySelector('.paragraph');
